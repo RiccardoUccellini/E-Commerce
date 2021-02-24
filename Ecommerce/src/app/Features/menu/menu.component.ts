e78e263bb64bba48a4769eee5e7a87662f4990f9
contilorenzo <lorenzo.conti.bg@gmail.com>
2021-02-24 12:27:34 +0100

    menu ngif

--- Ecommerce/src/app/Features/menu/menu.component.ts
+++ Ecommerce/src/app/Features/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  isLoginRoute() {
+    let loginPath = true;
+    if (this.router.url === '/login' || this.router.url === '/registrati') {
+      loginPath = false;
+    }
+    return loginPath;
   }
+
+
 
 }
